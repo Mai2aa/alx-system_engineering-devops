1a6a0e136c7679ea84366dc62320f90642e4ec0c
Mai2aa <mysaadil0@gmail.com>
2023-12-23 03:06:43 +0200

    advanced

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -24,9 +24,11 @@ int main(void)
 {
     pid_t pid;
     char count = 0;
+
     while (count < 5)
     {
         pid = fork();
+
         if (pid > 0)
         {
             printf("Zombie process created, PID: %d\n", pid);
