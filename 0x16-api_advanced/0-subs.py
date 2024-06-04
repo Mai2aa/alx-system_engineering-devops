a18d56fdc72597fbf02031c09bc3437ff4a1aa76
Mai2aa <mysaadil0@gmail.com>
2024-06-04 22:50:23 +0200

    number of subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,15 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers 
+    """Returns the number of subscribers
     including : Non-Active users"""
     try:
         headers = {'User-Agent': 'MyRedditApp/1.0 by /u/Mai2aa'}
-        response = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit), headers=headers)
+        response = requests.get("https://www.reddit.com/r/{}/about.json"
+                                .format(subreddit), headers=headers)
         if response.status_code == 200:
             return response.json()['data']['subscribers']
         else:
             return 0
-    except:
+    except (requests.exceptions.RequestException, KeyError):
         return 0
