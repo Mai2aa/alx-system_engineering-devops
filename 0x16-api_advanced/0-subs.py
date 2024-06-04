b9cb1a7a67c9a2d356402b3b3b70b4ba35ba75af
Mai2aa <mysaadil0@gmail.com>
2024-06-04 23:03:09 +0200

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,14 +8,11 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers
     including : Non-Active users"""
-    try:
-        response = requests.get(
-            "https://www.reddit.com/r/{}/about.json".format(subreddit),
-            headers={'User-Agent': 'My-User-Agent'},
-            allow_redirects=False)
-        if response.status_code == 200:
-            return response.json()['data']['subscribers']
-        else:
-            return 0
-    except (requests.exceptions.RequestException, KeyError):
+    response = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={'User-Agent': 'My-User-Agent'},
+        allow_redirects=False)
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    else:
         return 0
