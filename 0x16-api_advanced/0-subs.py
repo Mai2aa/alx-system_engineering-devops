8f9e64456b8ad735176afb143f4c371b8a16d0a9
Mai2aa <mysaadil0@gmail.com>
2024-06-04 23:00:21 +0200

    How many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,10 +9,9 @@ def number_of_subscribers(subreddit):
     """Returns the number of subscribers
     including : Non-Active users"""
     try:
-        headers = {'User-Agent': 'My-User-Agent'}
         response = requests.get(
             "https://www.reddit.com/r/{}/about.json".format(subreddit),
-            headers=headers,
+            headers={'User-Agent': 'My-User-Agent'},
             allow_redirects=False)
         if response.status_code == 200:
             return response.json()['data']['subscribers']
